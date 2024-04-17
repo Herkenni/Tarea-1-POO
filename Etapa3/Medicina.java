a6059f68c6dd1d649f4815ea9bb230adf482dd3c
Herkenni <103619618+Herkenni@users.noreply.github.com>
2024-04-17 12:03:00 -0400

    Update Medicina.java

--- Etapa3/Medicina.java
+++ Etapa3/Medicina.java
@@ -11,9 +11,12 @@ public class Medicina extends Item{
         this.salud=40;
         this.felicidad=0;
     }
+    
+    //La acción usando se encargara de emitir un mensaje y a su vez disminuir la cantidad del Item en 1
     @Override
     public void usando(){
         System.out.println("Aplicando medicamento "+this.nombre+"...");
+        this.cantidad=this.cantidad-1;
     }
     
 }
