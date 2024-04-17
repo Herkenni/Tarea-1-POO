e19df484e42b0833e114d020aff93c298cb108c9
a1ons00 <166877391+a1ons00@users.noreply.github.com>
2024-04-16 22:51:42 -0400

    paso del tiempo ejemplo

--- Etapa1/Main.java
+++ Etapa1/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.swing.Timer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*
         String nombre: Nombre de la mascota, todavia por arreglar
         Mascota mascota: Una clase Mascota llamada mascota que se usara durante el juego
@@ -29,7 +29,21 @@ public class Main {
         //inicializado todos los aspectos/atributos y mostrados, se procede a que comience a pasar el tiempo
         Timer timer= new Timer(1000, mascota);//interrupcion cada 1 segundo que activara el actionperformed
         timer.start();//iniciamos el timer
+        Thread.sleep(5000)//5 segundos de tiempo
 
+        //se observa que pasó el tiempo
+        mascota.Estado();
+        System.out.println("Mascota Virtual");
+        System.out.println("Atributos");
+        System.out.println("------------");
+        System.out.println("Nombre: "+mascota.getNombre());
+        System.out.println("Edad: "+mascota.getEdad());
+        System.out.println("Salud: "+mascota.getSalud());
+        System.out.println("Energía: "+mascota.getEnergia());
+        System.out.println("Felicidad: "+mascota.getFelicidad());
+        System.out.print("Estado: ");
+        mascota.getEstado();
+            
         
     }  
 }
