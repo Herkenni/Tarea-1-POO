970c9b8590c3cf3e48d6aa6ed398772451fb456a
a1ons00 <166877391+a1ons00@users.noreply.github.com>
2024-04-16 20:54:26 -0400

    Inventario.java  implemetaciones

--- Etapa3/Inventario.java
+++ Etapa3/Inventario.java
@@ -1,1 +1,30 @@
+public class Inventario extends Item{
 
+  private Item[] inventario;
+  private int Largo;//tamaño del inventario
+
+  //constructores
+    public Inventario(int largo){
+        inventario = new Item[largo];
+    }
+
+  //getters y setters
+  public Item[] getInventario(){
+    return inventario;
+  }
+
+  public int getLargo(){
+    return largo;
+  }
+
+  //metodo para crear lista con todo el inventario
+  public void AgregarItem(Item item){//agrega item al inventario
+  }
+
+//metodo para obtener el largo que tendrá el inventario
+  public void LargoInventario(Item item){
+  }
+  
+
+  
+}
