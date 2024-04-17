ce84edf6d24f0a3ebb427d18090c7e5ce879149e
Herkenni <103619618+Herkenni@users.noreply.github.com>
2024-04-17 12:01:04 -0400

    Update Main.java

--- Etapa3/Main.java
+++ Etapa3/Main.java
@@ -1,4 +1,5 @@
 package Etapa3;
+
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -10,69 +11,109 @@ public class Main {
         String nombre: Nombre de la mascota, todavia por arreglar
         Mascota mascota: Una clase Mascota llamada mascota que se usara durante el juego
         Scanner leer: Una variable de lectura para los comandos que hace el usuario
+        Double tiempo: Es el tiempo que transcurre con cada acción que pasa
+        Item Objeto: Es una variable default que simplemente guardara temporalmente un valor de la lista de objetos para poder usarlo y modificarlo
+        Int desicion: Es la desicion que toma el usuario sobre que objeto utilizar entre la lista que se le muestra
         */
         String nombre;
         Mascota mascota = new Mascota();
         Scanner leer = new Scanner(System.in);
+        double tiempo=0.0;
+        Item objeto;
+        int desicion=0;
+        
         //Se inician los parametros de mascota
         mascota.Inicio();
-        //Una lista que almacena los Items que se van creando
-        ArrayList<Item> lista_items = new ArrayList<>();
-        //Se establecen las variables Item que nos dijieron y se agregan a la lista lista_items (Agregando el nombre), la id y la inicialización de las estadisticas
-        Item Queso = new Comida();
-        Queso.setnombre("Queso");
-        Queso.setid(1);
-        Queso.inicio();
-        Item Jarabe = new Medicina();
-        Jarabe.setnombre("Jarabe");
-        Jarabe.setid(2);
-        Jarabe.inicio();
+        
+        //Una clase Inventario llamado inventario que almacena los Items que se van creando en una lista arraylist de Items
+        Inventario inventario = new Inventario();
+        //Se establecen las variables Item que nos dijieron y se agregan a la la clase inventario (Agregando el nombre), la id, cantidad y la inicialización de las estadisticas
         Item Pelota = new Juguete();
         Pelota.setnombre("Pelota");
-        Pelota.setid(3);
+        Pelota.setid(1);
+        Pelota.setcantidad(4);
         Pelota.inicio();
-        //Se agregan a la lista los Items creados
-        lista_items.add(Queso);
-        lista_items.add(Jarabe);
-        lista_items.add(Pelota);
-        /*Lo siguiente se muestran todas las caracteristicas de la mascota*/
+        
+        Item Queso = new Comida();
+        Queso.setnombre("Queso");
+        Queso.setid(2);
+        Queso.setcantidad(5);
+        Queso.inicio();
+        
+        Item Pan = new Comida();
+        Pan.setnombre("Pan");
+        Pan.setid(3);
+        Pan.setcantidad(3);
+        Pan.inicio();
+        
+        Item Jarabe = new Medicina();
+        Jarabe.setnombre("Jarabe");
+        Jarabe.setid(4);
+        Jarabe.setcantidad(4);
+        Jarabe.inicio();
+        
+        //Se agregan a la clase inventario los objetos que seran despues agregados a la ArrayList que tiene la clase
+        inventario.AgregarItem(Pelota);
+        inventario.AgregarItem(Queso);
+        inventario.AgregarItem(Pan);
+        inventario.AgregarItem(Jarabe);
         System.out.println("Mascota Virtual");
-        System.out.println("Atributos");
-        System.out.println("------------");
-        System.out.println("Nombre: "+mascota.getNombre());
-        System.out.println("Edad: "+mascota.getEdad());
-        System.out.println("Salud: "+mascota.getSalud());
-        System.out.println("Energía: "+mascota.getEnergia());
-        System.out.println("Felicidad: "+mascota.getFelicidad());
-        System.out.print("Estado: ");
-        //Se recorre la lista de Items y se va interactuando con ellos
-        for(Item aux: lista_items){
-            mascota.Aumentar(aux);
-            aux.usando();
+        
+        //Mientras desicion no tenga un valor igual a 9 no se detendra el while
+        while(desicion!=9){
+            //Se aumenta el tiempo en 0.5 por cada desicion a tomar
+            tiempo=tiempo+0.5;
+            //Se disminuyen las estadisticas de la mascota y aumenta su edad, para despues ver el Estado de la mascota con las estadisticas que quedo
+            mascota.Vejez();
+            mascota.Estado();
+            /*Lo siguiente se muestran todas las caracteristicas de la mascota*/
+            System.out.println("Tiempo simulado "+tiempo);
             System.out.println("Atributos");
             System.out.println("------------");
             System.out.println("Nombre: "+mascota.getNombre());
             System.out.println("Edad: "+mascota.getEdad());
             System.out.println("Salud: "+mascota.getSalud());
-            System.out.println("Energía: "+mascota.getEnergia());
+            System.out.println("Energia: "+mascota.getEnergia());
             System.out.println("Felicidad: "+mascota.getFelicidad());
             System.out.print("Estado: ");
             mascota.Humor();
             mascota.getEstado();
+            
+            //Las acciones que puede realizar el usuario
+            System.out.println("Acciones");
+            System.out.println("------------");
+            System.out.println("0: Dormir");
+            
+            //Se hace un recorrido de la lista de inventario lista_items, para mostrar su ID, Nombre y la cantidad del objeto
+            for(int j = 0; j<inventario.TamañoLista();j++){
+                objeto=inventario.ObtenerItem(j);
+                System.out.println(objeto.getid()+": "+objeto.getnombre()+", cantidad "+objeto.getcantidad());
+            }
+            
+            //Se espera la respuesta del usuario para ser almacenado en la variable desicion
+            System.out.print("Seleccione un elemento del inventario: ");
+            desicion=leer.nextInt();
+            
+            //Se busca denuevo en la lista de inventario algun item ID que coincida con la variable desicion para usarlo y aumentar las estadisticas de la mascota para despues disminuir la cantidad
+            for(int j = 0; j<inventario.TamañoLista();j++){
+                objeto=inventario.ObtenerItem(j);
+                if(objeto.getid()==desicion){
+                    objeto.usando();
+                    mascota.Aumentar(objeto);
+                    //Si la cantidad es igual a 0, se elimina el objeto del inventario
+                    if(objeto.getcantidad()== 0){
+                        inventario.EliminarItem(j);
+                        break;
+                    }else{
+                        inventario.ModificarItem(j, objeto);
+                        break;
+                    }
+                    //Si la primera consulta no es igual, se consultara por si acaso que si la desicion es dormir
+                }else if(desicion==0){
+                    mascota.Dormir();
+                }
+            }
+            
         }
-        //Se aplica la acción Dormir de la mascota por ultimo
-        mascota.Dormir();
-        System.out.println("Mascota Virtual");
-        System.out.println("Atributos");
-        System.out.println("------------");
-        System.out.println("Nombre: "+mascota.getNombre());
-        System.out.println("Edad: "+mascota.getEdad());
-        System.out.println("Salud: "+mascota.getSalud());
-        System.out.println("Energía: "+mascota.getEnergia());
-        System.out.println("Felicidad: "+mascota.getFelicidad());
-        System.out.print("Estado: ");
-        mascota.Humor();
-        mascota.getEstado();
-        System.out.print("Estado: ");
     }  
 }
