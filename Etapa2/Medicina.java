d50cbaaa5453d9c2317a0e8afbab279f50ff12c6
a1ons00 <166877391+a1ons00@users.noreply.github.com>
2024-04-16 20:14:49 -0400

    agregue atributos de medicina(seran los derivados de la clase padre ITEM)

--- Etapa2/Medicina.java
+++ Etapa2/Medicina.java
@@ -3,6 +3,12 @@ package Etapa2;
 Es el hijo de Item
 */
 public class Medicina extends Item{
+
+
+    public Medicina(String nombre, int id, int energia, int salud, int felicidad, int cantidad){
+        super(nombre,id,energia,salud,felicidad,cantidad);
+    }
+
     
     //Las estadisticas que entrega la Medicina
     @Override
