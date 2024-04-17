7cff7a867ff7faf687cd01e20f7b265800e37b0a
Herkenni <103619618+Herkenni@users.noreply.github.com>
2024-04-17 11:58:38 -0400

    Update Inventario.java

--- Etapa3/Inventario.java
+++ Etapa3/Inventario.java
@@ -1,39 +1,36 @@
-public class Inventario extends Item{
+package Etapa3;
 
-  private Item[] inventario;
-  private int Largo;//tamaño del inventario
+//Clase inventario
 
-  //constructores
-    public Inventario(int largo){
-        inventario = new Item[largo];
+import java.util.ArrayList;
+
+public class Inventario {
+    //Arraylist<Item> lista_items: Consiste en una lista Arraylist que almacena todos los Items que se van registrando
+    ArrayList<Item> lista_items = new ArrayList<>();
+    
+    //Funcion que recibe un Item y lo almacena en la lista lista_items
+    public void AgregarItem(Item item){
+        this.lista_items.add(item);
     }
-
-  //getters y setters
-  public Item[] getInventario(){
-    return inventario;
-  }
-
-  public int getLargo(){
-    return largo;
-  }
-
- public void setLargo(int Largo){
-  this.Largo=Largo;
-}
-
-
-  //metodo para crear lista con todo el inventario
-//agrega item al inventario public void
-//se usa una vez definido el inventario
- AgregarItem(Item item){
- 
- }
-
-//metodo para obtener el largo que tendrá el inventario
-  public void LargoInventario(Item item){
-
-  }
-  
-
-  
+    
+    /*Funcion que recibe un int y un Item, busca en la lista el puesto i, para modificarlo con Item
+    más que nada para actualizar valores*/
+    public void ModificarItem(int i, Item c){
+        lista_items.set(i, c);
+    }
+    
+    //Función que entrega el tamaño de la lista lista_items
+    public int TamañoLista(){
+        return this.lista_items.size();
+    }
+    
+    //Función que entrega un Item que esta ubicado en el valor de int dato
+    public Item ObtenerItem(int dato){
+        return this.lista_items.get(dato);
+    }
+    
+    //Función que se encarga de eliminar un dato que esta ubicado en el valor int dato
+    public void EliminarItem(int dato){
+        this.lista_items.remove(dato);
+    }
 }
