32d38fc2a25651f6b192ef1c83c96e2e2826fbf6
a1ons00 <166877391+a1ons00@users.noreply.github.com>
2024-04-19 19:43:16 -0400

    cambios finales a mascota etapa 2

--- Etapa1/Mascota.java
+++ Etapa1/Mascota.java
@@ -3,7 +3,7 @@ import java.awt.event.*; ///libreria que contiene interfaz actionListener
 /*
   Mascota
  */
-public class Mascota implements ActionListener {
+public class Mascota {
 
     /*
     String nombre: Nombre de la mascota
@@ -14,21 +14,24 @@ public class Mascota implements ActionListener {
     EnumEstado Estado: Es una enumeración con todos los estados de la Mascota
     */
     protected String nombre;
-    protected int edad, salud, energia, felicidad;
+    protected int salud, energia, felicidad;
     protected EnumEstado Estado;
+    protected double tiempoSimulacion,edad; //se coloca en las mismas unidades que el tiempo ya que eso piden en el enunciado
 
     //metodos constructores
 
     public Mascota(){ ///constructor para "setear" una mascota
+ 
     }
     //Iniciar una mascota con las estadisticas principales que nos declaran
     public void Inicio(){ //constructor para crear una mascota parametrizada
         this.nombre="Garfield";
         this.edad=0;
-        this.salud=100;
-        this.energia=100;
+        this.salud=50;
+        this.energia=50;
         this.felicidad=50;
         this.Estado=EnumEstado.NEUTRO;
+        this.tiempoSimulacion=0;
     }
 
     ///metodos setters
@@ -37,7 +40,7 @@ public class Mascota implements ActionListener {
         this.nombre = nombre;
     }
 
-    public void setEdad(int edad) {
+    public void setEdad(double edad) {
         this.edad = edad;
     }
 
@@ -55,7 +58,7 @@ public class Mascota implements ActionListener {
 
     //metodos getters
 
-    public int getEdad() {
+    public double getEdad() {
         return this.edad;
     }
 
@@ -74,6 +77,11 @@ public class Mascota implements ActionListener {
     public int getSalud() {
         return this.salud;
     }
+
+    public double getTiempo(){ //tiempo de simulacion 
+        return this.tiempoSimulacion;
+    }
+
     //Dependiento del valor de Estado, se obtiene un valor
     public void getEstado(){
         switch(this.Estado){
@@ -97,71 +105,92 @@ public class Mascota implements ActionListener {
                 break;
             case MUERTO:
                 System.out.println("(x_x) fin del juego");
+                System.exit(0); ///IMPORTANTE: se cierra el programa cuando la mascota muere
                 break;      
         }
     }
 
     ///metodos
-
-    public void Estado(){
-        if(felicidad>=60){
-            Estado=EnumEstado.FELIZ;
+    //El metodo de dormir que tiene la mascota que aumenta la energia a 100
+    public void Dormir(){
+        this.energia=100;
+        this.salud=this.salud+15;
+        salud=(salud>100)?100:salud; //limite de valor de salud, ya que sin esto se iba al infinito
+        System.out.println(this.nombre+" ha dormido como un tronco");
+    }
+    
+    //Aumenta las estadisticas de la mascota segun el Item elegido, y si supera 100 lo deja en 100
+    public void Aumentar(Item item){
+        this.energia= this.energia + item.getenergia();
+        if(this.energia>100){
+            this.energia = 100;
         }
-
-        else if(felicidad<=20){
-            Estado=EnumEstado.TRISTE;
+        this.felicidad= this.felicidad + item.getfelicidad();
+        if(this.felicidad>100){
+            this.felicidad = 100;
         }
-
-        else if((salud<=20 && edad<=5) || (salud<=50 && edad>=5 && edad<=10)){
-            Estado=EnumEstado.HAMBRIENTO;
-        }
-
-        else if(edad>5 && salud<=30 && energia<=30){
-            Estado=EnumEstado.CANSADO;
-        }
-
-        else if((salud==0 || energia==0) || edad>=15){
-            Estado=EnumEstado.MUERTO;
+        this.salud= this.salud + item.getsalud();
+        if(this.salud>100){
+            this.salud = 100;
         }
     }
-
-
-
-  //debemos implementar actionperformed en nuestra clase y tambien en el main. 
-  // Cada vez que ocurre la interrupcion/evento se ejecuta el metodo actionPerformed que está
-  //Timer creado en main se le asocia el objeto que contiene el metodo actionperformed
-  
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    //Establece el valor de la variable Estado dependiendo de las estadisticas de la mascota
+    public void Humor(){
+        if(this.salud==0 || this.energia==0 || this.edad >= 15){
+            this.Estado=EnumEstado.MUERTO;
+        }else if(this.energia<= 15){
+            this.Estado=EnumEstado.CANSADO;
+        }else if(this.edad>5 && this.salud<=30 && this.energia<=30){
+            this.Estado=EnumEstado.ENOJADO;
+        }else if(this.salud<=20 && this.edad<=5 || this.salud<=50 && this.edad>=5 &&this.edad <=10){
+            this.Estado=EnumEstado.HAMBRIENTO;
+        }else if(this.felicidad<=20){
+            this.Estado=EnumEstado.TRISTE;
+        }else if(this.felicidad>=60){
+            this.Estado=EnumEstado.FELIZ;
+        }else{
+            this.Estado=EnumEstado.NEUTRO;
+        }
+    }
+    
+    //Disminuye las estadisticas de la mascota y aumenta la edad de la misma una cantidad fija
+    public void Vejez(){
+        this.edad=this.edad+0.5;
+        this.salud=this.salud-5;
+        this.energia=this.energia-5;
+        this.felicidad=this.felicidad-5;
+    }
+    
+    //se utiliza cada vez que se interactua con un item,
+    //ya que segun el enunciado solo con ello pasa el tiempo
+    public void aumentarTiempo(){
+        this.tiempoSimulacion=this.tiempoSimulacion+0.5;
+      }
+    
+    //Se encarga de revisar las estadisticas de la mascota y actuar en base a ellos
+    //se debe aplicar despues de ejecutar la funcion aumentarTiempo()
+    public void Estado() {
         //condiciones si los atributos estan en cierto nivel y sus consecuencias 
         if (salud<=10) {
-            felicidad=felicidad-20;
+            felicidad=felicidad-20*(int)this.tiempoSimulacion;
             
         }
           
         else if (salud<=50 && edad>5 && edad<=10) {
-            energia=energia-20;
-            felicidad=felicidad-20;
+            energia=energia-20*(int)this.tiempoSimulacion;
+            felicidad=felicidad-20*(int)this.tiempoSimulacion;
         }
 
         else if (salud<=50 && edad>10) {
-            energia=energia-20;
-            felicidad=felicidad-30;  
-        }
-        else{ //solo va disminuyendo gradualmente la energia y la felicidad
-          energia--;
-          felicidad--;
-          salud--;
+            energia=energia-20*(int)this.tiempoSimulacion;
+            felicidad=felicidad-30*(int)this.tiempoSimulacion;  
         }
       
-      //condicional simple para pooner como limite que la energia y felicidad minimo pueden ser 0.
+      //condicional simple para pooner como limite que la energia, felicidad y salud minimo pueden ser 0.
         energia=(energia<0)?0:energia; 
-        felicidad=(felicidad<0)?0:felicidad;
-        edad++;//va pasando la edad de la mascota de acuerdo al tiempo de simulacion
-        
+        felicidad=(felicidad<0)?0:felicidad; 
+        salud=(salud<0)?0:salud;
    }
 
-
-  
 
 }
